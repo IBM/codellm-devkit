501df5041453c5e15198aac8241bdf5d1bac598a
Rahul Krishna <i.m.ralk@gmail.com>
2024-10-08 15:12:39 -0400

    Update dependencies in pyproject.toml
    Signed-off-by: Rahul Krishna <i.m.ralk@gmail.com>
    Signed-off-by: John Rofrano <johnnyroy@johnrofrano.com>

--- cldk/models/python/models.py
+++ cldk/models/python/models.py
@@ -1,12 +1,3 @@
-from typing import List
-from pydantic import BaseModel
-
-
-class PyArg(BaseModel):
-    arg_name: str
-    arg_type: str
-
-
 class PyImport(BaseModel):
     from_statement: str
     imports: List[str]
@@ -22,24 +13,6 @@ class PyCallSite(BaseModel):
     end_column: int
 
 
-class PyMethod(BaseModel):
-    code_body: str
-    method_name: str
-    full_signature: str
-    num_params: int
-    modifier: str
-    is_constructor: bool
-    is_static: bool
-    formal_params: List[PyArg]
-    call_sites: List[PyCallSite]
-    return_type: str
-    class_signature: str
-    start_line: int
-    end_line: int
-    # incoming_calls: Optional[List["PyMethod"]] = None
-    # outgoing_calls: Optional[List["PyMethod"]] = None
-
-
 class PyClass(BaseModel):
     code_body: str
     full_signature: str
@@ -49,17 +22,53 @@ class PyClass(BaseModel):
     methods: List[PyMethod]
 
 
+class PyMethod(BaseModel):
+    code_body: str
+    method_name: str
+    full_signature: str
+    is_constructor: bool
+    is_static: bool
+    formal_params: List[PyArg]
+    call_sites: List[PyCallSite]
+    return_type: str
+    class_signature: str
+    start_line: int
+    end_line: int
+
+
 class PyModule(BaseModel):
     qualified_name: str
     functions: List[PyMethod]
     classes: List[PyClass]
     imports: List[PyImport]
-    #expressions: str
+
+
+class PyArg(BaseModel):
+    arg_name: str
+    arg_type: str
 
 
 class PyBuildAttributes(BaseModel):
     """Handles all the project build tool (requirements.txt/poetry/setup.py) attributes"""
 
+    # Name of the build file (e.g., 'setup.py', 'requirements.txt', 'pyproject.toml')
+    build_file_type: str
+    code_body: str
+    # Build tool used (e.g., 'setuptools', 'poetry', 'pip')
+    build_tool: str
+    package_name: str = None
+    version: str = None
+    dependencies: List[str] = []
+    dev_dependencies: List[str] = []
+    scripts: List[str] = []
+
 
 class PyConfig(BaseModel):
     """Application configuration information"""
+
+    config_file_names: Dict[str, str]  # Name of the configuration file
+    code_body: str  # The content of the configuration file
+    config_type: str  # Type of config file (e.g., 'json', 'yaml', 'ini', 'python')
+    settings: Dict[str, Any] = {}  # Parsed settings from the config file
+    start_line: int = None
+    end_line: int = None
